338e72b7effb36f967dd24fb803b77a8252d8f6e
Schascha <contact.schascha@gmail.com>
2021-05-24 22:09:59 +0200

    Add pwa config

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,13 @@ module.exports = {
 
 	publicPath: process.env.NODE_ENV === 'production' ?
 		'/lava/' :
-		'/'
+		'/',
+
+	pwa: {
+		name: 'lava',
+		themeColor: '#141b1f',
+		manifestOptions: {
+			background_color: '#141b1f'
+		}
+	}
 };
