f61b1e99315e6530030639d2e2a9559350084263
Schascha <contact.schascha@gmail.com>
2021-05-24 19:24:07 +0000

    Deploying to gh-pages from  @ 572bd332faa476666f1ac2be6d00030032dc2866 🚀

---
+++ precache-manifest.5838f534fbf59f32ad24954e8811b4e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006a9a0996591299be73",
+    "url": "/lava/css/app.39ef08cb.css"
+  },
+  {
+    "revision": "694ac68ba23714a4cd6ed947aa533704",
+    "url": "/lava/index.html"
+  },
+  {
+    "revision": "006a9a0996591299be73",
+    "url": "/lava/js/app.2396bbce.js"
+  },
+  {
+    "revision": "9f17db5d64efa86ab303",
+    "url": "/lava/js/chunk-vendors.e329ed66.js"
+  },
+  {
+    "revision": "58c81a5d2ee407876f52edd97d8d281b",
+    "url": "/lava/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lava/robots.txt"
+  }
+]);
